ebb40eb14c0aa86f3396d1351514c02990f2ea74
robrobyytt <aminebessaou11@gmail.com>
2025-01-01 02:12:16 +0100

    Create My-girl.py
    Yy

---
+++ My-girl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print("• created by Robx777 •")
+
+import smtplib
+from os import system
+
+def main():
+        print("Invalid option, please try again.")
+
+if __name__ == "__main__":
+    main()
